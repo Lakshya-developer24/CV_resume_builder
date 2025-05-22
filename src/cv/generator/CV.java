e624b6ede28dfa36e9a6e0c25eb3d1ab089a7ed0
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-05-22 20:25:27 +0530

    Update CV.java

--- src/cv/generator/CV.java
+++ src/cv/generator/CV.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * And open the template in the editor.
  *this will give a well placed and organized cv
  */
 
