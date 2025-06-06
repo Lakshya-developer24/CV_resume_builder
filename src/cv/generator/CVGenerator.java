af74701e4945e1b17f2224682288079349f76f4c
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-06-06 22:39:24 +0530

    Update CVGenerator.java

--- src/cv/generator/CVGenerator.java
+++ src/cv/generator/CVGenerator.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * And Open The Template In The editor.
+ * And Open The Template In The Editor.
  */
 package cv.generator;
 
